811d8239223925194813b6dac05c9dc44affb9f5
Sergei Shlegel <serg-69@mail.ru>
2022-12-20 10:15:00 +0300

    modifed to view binding

--- app/src/main/java/com/example/qualifierslesson/MainActivity.kt
+++ app/src/main/java/com/example/qualifierslesson/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.qualifierslesson
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.qualifierslesson.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bindingClass: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
     }
 }
